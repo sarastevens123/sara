555e37c94bfb170fe190c119dcf4421fe6ec666d
Sara Stevens <sarastevens123@gmail.com>
2023-10-06 21:18:28 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import './App.css';
+import './Style.css'
+
+
+function App() {
+  return (
+    <div>
+
+    
+    <Header />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+
+import Button from './Button'
+
+
+
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>Fuck Off</h1>
+            
+            <Button />
+            <Button />
+            <Button />
+        </header>
+    );
+}
+
+export default Header;
